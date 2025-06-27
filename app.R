9bd56e2266caa08de09ae09eefa5771705b79e50
gnoblet <gnoblet@zaclys.net>
2025-06-27 15:58:01 +0200

    install all desc packages in app.R

--- app.R
+++ app.R
@@ -1,7 +1,11 @@
 # Minimal app.R for Golem on Posit Cloud
-if (!requireNamespace("golem", quietly = TRUE)) {
-  install.packages("golem")
-}
+
+required_packages <- c(
+  "golem", "pkgload", "shiny", "bslib", "DT", "dplyr", "ggiraph", "ggplot2", "glue", "htmltools", "kableExtra", "knitr", "lubridate", "magrittr", "readr", "rlang", "rmarkdown", "scales", "stringr", "tidyr", "viridis"
+)
+to_install <- setdiff(required_packages, rownames(installed.packages()))
+if (length(to_install) > 0) install.packages(to_install)
+
 library(golem)
 if (requireNamespace("pkgload", quietly = TRUE)) {
   pkgload::load_all()

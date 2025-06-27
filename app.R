e940e43417ea00ed38b6af2bfd5a659612a4a769
gnoblet <gnoblet@zaclys.net>
2025-06-27 15:53:21 +0200

    Check namespace

--- app.R
+++ app.R
@@ -1,5 +1,8 @@
-library(golem)
 # Minimal app.R for Golem on Posit Cloud
+if (!requireNamespace("golem", quietly = TRUE)) {
+  install.packages("golem")
+}
+library(golem)
 if (requireNamespace("pkgload", quietly = TRUE)) {
   pkgload::load_all()
 }
